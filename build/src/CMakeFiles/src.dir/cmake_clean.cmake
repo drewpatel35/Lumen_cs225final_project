e67411a8babc691ac1333523073826b2ab924779
sninic <89553559+sninic@users.noreply.github.com>
2022-11-27 23:03:12 -0600

    Add files via upload

---
+++ build/src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/open_flights.cpp.o"
+  "CMakeFiles/src.dir/open_flights.cpp.o.d"
+  "libsrc.a"
+  "libsrc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
