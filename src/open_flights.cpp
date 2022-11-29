b60b130e2d294378d456be55a6f7b42e478635da
sninic <89553559+sninic@users.noreply.github.com>
2022-11-28 23:11:51 -0600

    Update open_flights.cpp

--- src/open_flights.cpp
+++ src/open_flights.cpp
@@ -1,1 +1,17 @@
 # include "open_flights.h"
+
+using namespace std;
+
+AirportMap::airportMap(string airports, string routes){
+    
+}
+
+void AirportMap::insertAirport(vector<string> line) {
+
+}
+void AirportMap::insertRoute(vector<string> line) {
+
+}
+double AirportMap::findDistance(int start, int dest) {
+
+}
