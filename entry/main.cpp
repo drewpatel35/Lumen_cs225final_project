c0989b324bc8f2ed66c3a9116c1c83bfcc95d6cb
sninic <89553559+sninic@users.noreply.github.com>
2022-11-30 01:31:19 -0600

    Update main.cpp

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,5 +1,8 @@
 #include "open_flights.h"
 
 int main() {
-    return 0;
+    AirportMap test("/workspaces/cs225/MPs/release-f22/Lumen_cs225final_project-main/entry/airports.dat",
+                    "/workspaces/cs225/MPs/release-f22/Lumen_cs225final_project-main/entry/routes.dat");
+    test.printAirports();
+    test.displayAdjList();
 }
